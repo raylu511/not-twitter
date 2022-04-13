2d342f8f40c4f1d99dfdedaab9090acfcb6f2ba6
raylu511 <raylu511@gmail.com>
2022-04-13 12:27:20 -0400

    Added new seeds

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -13,8 +13,23 @@ exports.seed = async function(knex) {
   
   await knex('blogs').del()
   await knex('blogs').insert([
-    {user_id: "1", body: `Shawn's Blog`},
-    {user_id: "2", body: `Jarae's Blog`},
-    {user_id: "3", body: `Ray's Blog`},
+    {user_id: 1, body: `Shawn's Blog`},
+    {user_id: 2, body: `Jarae's Blog`},
+    {user_id: 3, body: `Ray's Blog`},
+  ]);
+  await knex('comments').del()
+  await knex('comments').insert([
+    {user_id: 1, blog_id: 2, comment_text: "Nice Blog FROM Shawn!"},
+    {user_id: 1, blog_id: 3, comment_text: "Nice Blog From Shawn!"},
+    {user_id: 2, blog_id: 1, comment_text: "Nice Blog From Jarae!"},
+    {user_id: 3, blog_id: 2, comment_text: "Nice Blog From Ray!"},
+    
+  ]);
+  await knex('likes').del()
+  await knex('likes').insert([
+    {user_id: 1, blog_id: 2},
+    {user_id: 1, blog_id: 3},
+    {user_id: 2, blog_id: 1},
+    {user_id: 3, blog_id: 2},
   ]);
 };
