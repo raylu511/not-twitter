a35ecab7dbf0753ae12fea98220a23ddc9af51e5
raylu511 <raylu511@gmail.com>
2022-04-14 10:13:58 -0400

    Added blog id to each blogDiv

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,1 +1,6 @@
-console.log("yo")
+const blogs = document.getElementsByClassName("blog");
+for (let blog of blogs) {
+  blog.addEventListener("click", (e) => {
+    console.log("hello world");
+  });
+}
